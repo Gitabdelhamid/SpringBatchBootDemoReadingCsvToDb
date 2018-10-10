1fd812796c27c993e1b12b3e0b09d6882032d312
dell-PC\dell <ghailan.abdelhamid@gmail.com>
2018-10-11 00:04:22 +0200

    first commit

--- src/main/java/com/techprimers/springbatchexample1/SpringBatchExample1Application.java
+++ src/main/java/com/techprimers/springbatchexample1/SpringBatchExample1Application.java
@@ -2,7 +2,11 @@ package com.techprimers.springbatchexample1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author dell
+ *
+ */
 @SpringBootApplication
 public class SpringBatchExample1Application {
 
